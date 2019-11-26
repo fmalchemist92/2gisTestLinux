e9daea13e6698411274bb0888d2681a4e666df9f
Yaroslav Slabienko <fmalchemist92@ya.ru>
2019-11-26 16:56:46 +0700

    comments rewiev

--- 2gisTestLinux/FileAnalizer.h
+++ 2gisTestLinux/FileAnalizer.h
@@ -4,10 +4,8 @@
 class FileAnalizer {
 public:
 	FileAnalizer(const std::string& path);
-	unsigned int wordsCounter(
-		const std::string& countingWord,
-		const std::string& separators = " .,?!;:()'…–\"\t") const; //счётчик слов, принимает искомое слово , чувствительность к региструи строку символов разделителей в качестве аргументов, возвращает количество найденных слов
-	uint32_t checksum32() const; // возвращает 32-хбитную чексумму, рассчитанную по алгоритму checksum = word1 + word2 + … +wordN (word1..wordN – 32-хбитные слова, представляющие содержимое файла)
+	unsigned int wordsCounter(const std::string& countingWord, const std::string& separators = " .,?!;:()'…–\"\t") const; //функция чувствительна к регистру
+	uint32_t checksum32() const; 
 
 private:
 	std::string inputFilePath_;
