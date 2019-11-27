984026cfbc4ea4f12b6edaf1c053d92f7daa39e5
Yaroslav Slabienko <fmalchemist92@ya.ru>
2019-11-27 18:28:16 +0700

    comment

--- 2gisTestLinux/FileAnalizer.h
+++ 2gisTestLinux/FileAnalizer.h
@@ -4,7 +4,7 @@
 class FileAnalizer {
 public:
 	FileAnalizer(const std::string& path);
-	unsigned int wordsCounter(const std::string& countingWord, const std::string& separators = " .,?!;:()'…–\"\t") const; //функция чувствительна к регистру
+	unsigned int wordsCounter(const std::string& countingWord, const std::string& separators = " .,?!;:()'…–\"\t") const; //нужно переделать
 	uint32_t checksum32() const; 
 
 private:
